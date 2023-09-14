50e8f4732ef14c7fdd6bd9a198b03be629f0a87c
rraheemp <89361408+rraheemp@users.noreply.github.com>
2023-09-13 22:30:47 -0400

    Create Collatz.c
    Children tell the user if the number in the current collatz step is even or odd

---
+++ Collatz.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(){
+
+    int number;
+    pid_t child1, child2;
+
+    printf("Please enter a number: ");
+    scanf("%d", &number);
+
+    printf("collatz %d\n", number);
+    printf("This is the Parent waiting!\n");
+
+    while(number != 1){
+        if (number % 2 == 0) {
+            //this is the first child process
+            child1 = fork();
+            if(child1 == 0) {
+                printf("From Child1, pid=%d: number=%d is even\n", getpid(), number);
+                exit(0);
+            } else if (child1 < 0){
+                printf("Child1 was not forked successfully. \n");
+            }
+            wait(NULL);
+            number = number / 2;
+        } else {
+            //this is the second child process
+            child2 = fork();
+            if(child2 == 0) {
+                printf("From Child2, pid=%d: number=%d is odd\n", getpid(), number);
+                exit(0);
+            } else if (child2 < 0){
+                printf("Child2 was not forked successfully. \n");
+            }
+            wait(NULL);
+            number = 3 * number + 1;
+        }
+    }
+
+    printf("All my Children Complete\n");
+
+    return 0;
+}
